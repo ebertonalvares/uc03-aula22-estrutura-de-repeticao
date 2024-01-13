8c9351e788a748bc6de305237cc4295f6131ed38
eberton mauricio alvares <ebertonalvares@hotmail.com>
2024-01-12 21:49:57 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -37,11 +37,83 @@ const readline = require ('readline-sync');
  //}
 
 
+//for - para (inicializa var controle; condicao; incremento)
+//while - enquanto (condicao verdadeira)
+//do while - faça enquanto (condicao verdadeira)
+
+//while deve ser usado quando nao temos certeza da condicao e
+//nao precisamos executar pelo menos 1 vez 
+
+
+// do while vai ser executado pelo menos 1 vez para poder validar a condicao 
+
+//const valor = readline.questionInt("Informe um numero inteiro: ");
+
+//while(valor > 0){
+//    console.log("Valor maior que zero:")
+//    break;
+//}
+
+const senha = "123456"
+let senhaValida = false;//flag
+let vezes = 0;
+do{
+    const senhaDigitada = readline.questionNewPassword("Informe a senha")
+    if(senha === senhaDigitada){
+        console.log("Senha Valida")
+        senhaValida = true;
+    }else {
+        console.log("Senha Invalida")
+    }
+}while(!senhaValida)
+
+
+ //1-Crie um programa que peça 5 nomes diferentes e imprima na tela logo após o nome ser informado.
  
+//for(let i = 0; i <= 5; i++){
+//    const nome = readline.question("Informe um nome: ");
+//    console.log(nome);
+//}
+
+//2-Crie um programa que executa 10 vezes um bloco de código, onde é solicitado um valor numérico inteiro. 
+//Caso o valor informado seja impar, imprima impar no console, caso seja par, imprima par no console.
 
 
+//for (let i = 0;i <=10; i++){
+//    const numero = readline.questionInt("Informe um numero: ");
+//    numero2 = numero % 2 
+//    if (numero2 == 0 ){
+        
+//       console.log("Par:" );
+//  } else{
+        
+//    console.log("Impar:" );
+//  }
+    
+    
+//  }
 
-for(let i = 0; i <= 5; i++){
-    const nome = readline.question("Informe um nome: ");
-    console.log(nome);
-}
+//3-Imprima a tabuada de um número informado usando o while.
+
+//obter um numero
+
+//const numeroTabuada = readline.questionInt("Informe um numero inteiro para mostrar a tabuada: "); 
+
+//criar uma variavel de controle para o while
+//let vez = 1;
+
+//criar o while 
+
+//while(vez <= 10){
+//    const resultado = vez * numeroTabuada;
+//    console.log(`${numeroTabuada} X ${vez} = ${resultado}`);
+//    vez ++;
+//}
+//4-Imprima todos os valores impares entre 1 e 100 usando for;
+//for (let i =1; i <= 100; i++ ){
+//    if (i % 2 > 0){
+//        console.log(i)
+//    }
+//}
+
+
